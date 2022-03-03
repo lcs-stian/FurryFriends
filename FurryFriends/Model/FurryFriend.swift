049334a9c2ef77ad2087ae7f69442ce391fde229
Suzanne <STian@lcs.on.ca>
2022-03-03 11:58:40 -0500

    Added functions and use task structure

---
+++ FurryFriends/Model/FurryFriend.swift
@@ -0,0 +1,13 @@
+//
+//  FurryFriend.swift
+//  FurryFriends
+//
+//  Created by Suzanne Tian on 2022-03-03.
+//
+
+import Foundation
+
+struct FurryFriend: Decodable {
+    let message: String
+    let status: Int
+}
--- FurryFriends/Views/DogView.swift
+++ FurryFriends/Views/DogView.swift
@@ -10,17 +10,16 @@ import SwiftUI
 struct DogView: View {
     
     //MARK: Stored properties
+    @State var currentImage: FurryFriend = FurryFriend(message: "", status: 0)
     
-    @State var currentImage = URL(string: "https://www.russellgordon.ca/lcs/miscellaneous/transparent-pixel.png")!
-    
-    
+   
     //MARK: Computed properties
     
     var body: some View {
         
         VStack {
             
-            RemoteImageView(fromURL: currentImage)
+            Image(currentImage.message)
                 .padding(30)
             
             
@@ -41,11 +40,6 @@ struct DogView: View {
                 Spacer()
             }
             
-            List {
-                Image()
-                RemoteImageView(fromURL: currentImage)
-                
-            }
             
             
             .overlay(
@@ -62,27 +56,53 @@ struct DogView: View {
         
         
         
+        // When the app opens, get a new joke from the web service
         .task {
             
-            // Assemble the URL that points to the endpoint
-            let url = URL(string: "https://images.dog.ceo/breeds/labrador/lab_young.JPG/")!
+            // Load a joke from the endpoint!
+            // We "calling" or "invoking" the function
+            // named "loadNewJoke"
+            // A term for this is the "call site" of a function
+            // What does "await" mean?
+            // This just means that we, as the programmer, are aware
+            // that this function is asynchronous.
+            // Result might come right away, or, take some time to complete.
+            // ALSO: Any code below this call will run before the function call completes.
+            await loadNewJoke()
             
-            // Define the type of data we want from the endpoint
-            // Configure the request to the web site
-            var request = URLRequest(url: url)
-            // Ask for JSON data
-            request.setValue("application/json",
-                             forHTTPHeaderField: "Accept")
+            print("I tried to load a new joke")
             
-            // Start a session to interact (talk with) the endpoint
-            let urlSession = URLSession.shared
-            
-            
-            // Replaces the transparent pixel image with an actual image of an animal
-            // Adjust according to your preference ☺️
-            currentImage = URL(string: remoteDogImage)!
-            
+            //loading favourites from the local device storge
+            loadFavourites()
+        
         }
+        
+        
+        .navigationTitle("Dog")
+        .padding()
+    }
+    
+    // MARK: Functions
+    
+    // Define the function "loadNewJoke"
+    // Teaching our app to do a "new thing"
+    //
+    // Using the "async" keyword means that this function can potentially
+    // be run alongside other tasks that the app needs to do (for example,
+    // updating the user interface)
+    func loadNewJoke() async {
+        // Assemble the URL that points to the endpoint
+        let url = URL(string: "https://icanhazdadjoke.com/")!
+        
+        // Define the type of data we want from the endpoint
+        // Configure the request to the web site
+        var request = URLRequest(url: url)
+        // Ask for JSON data
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
+        
+        // Start a session to interact (talk with) the endpoint
+        let urlSession = URLSession.shared
         
         // Try to fetch a new joke
         // It might not work, so we use a do-catch block
@@ -96,7 +116,11 @@ struct DogView: View {
             //                                 DATA TYPE TO DECODE TO
             //                                         |
             //                                         V
-            currentJoke = try JSONDecoder().decode(DogView.self, from: data)
+            currentJoke = try JSONDecoder().decode(DadJoke.self, from: data)
+            
+            // Reset the flag that tracks whether the current joke
+            // is a favourite
+            currentJokeAddedToFavourites = false
             
         } catch {
             print("Could not retrieve or decode the JSON from endpoint.")
@@ -105,8 +129,73 @@ struct DogView: View {
             print(error)
         }
         
-        .navigationTitle("Dog")
-        .padding()
+    }
+    
+    // Saves (persists) the data to local storage on the device
+    func persistFavourites() {
+        
+        // Get a URL that points to the saved JSON data containing our list of tasks
+        let filename = getDocumentsDirectory().appendingPathComponent(savedFavouritesLabel)
+        
+        // Try to encode the data in our people array to JSON
+        do {
+            // Create an encoder
+            let encoder = JSONEncoder()
+            
+            // Ensure the JSON written to the file is human-readable
+            encoder.outputFormatting = .prettyPrinted
+            
+            // Encode the list of favourites we've collected
+            let data = try encoder.encode(favourites)
+            
+            // Actually write the JSON file to the documents directory
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+            
+            // See the data that was written
+            print("Saved data to documents directory successfully.")
+            print("===")
+            print(String(data: data, encoding: .utf8)!)
+            
+        } catch {
+            
+            print(error.localizedDescription)
+            print("Unable to write list of favourites to documents directory in app bundle on device.")
+            
+        }
+        
+    }
+    
+    // Loads favourites from local storage on the device into the list of favourites
+    func loadFavourites() {
+        
+        // Get a URL that points to the saved JSON data containing our list of favourites
+        let filename = getDocumentsDirectory().appendingPathComponent(savedFavouritesLabel)
+        print(filename)
+                
+        // Attempt to load from the JSON in the stored / persisted file
+        do {
+            
+            // Load the raw data
+            let data = try Data(contentsOf: filename)
+            
+            // What was loaded from the file?
+            print("Got data from file, contents are:")
+            print(String(data: data, encoding: .utf8)!)
+
+            // Decode the data into Swift native data structures
+            // Note that we use [DadJoke] since we are loading into a list (array)
+            // of instances of the DadJoke structure
+            favourites = try JSONDecoder().decode([DadJoke].self, from: data)
+            
+        } catch {
+            
+            // What went wrong?
+            print(error.localizedDescription)
+            print("Could not load data from file, initializing with tasks provided to initializer.")
+
+        }
+
+        
     }
 }
 
